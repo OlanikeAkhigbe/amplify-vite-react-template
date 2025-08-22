7837be7cf34e1ea5a9572b649cfe1a76168ed329
Olanike Akhigbe <olanike.akhigbe@accenture.com>
2025-08-22 18:39:49 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,41 +6,38 @@ import { generateClient } from "aws-amplify/data";
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [bookings, setBookings] = useState<Array<Schema["Booking"]["type"]>>([]);
   const { signOut } = useAuthenticator();
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+    client.models.Booking.observeQuery().subscribe({
+      next: (data) => setBookings([...data.items]),
     });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  function createBooking() {
+    client.models.Booking.create({ content: window.prompt("Booking content") });
   }
 
     
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+  function deleteBooking(id: string) {
+    client.models.Booking.delete({ id })
   }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>Meeting Room Bookings</h1>
+      <button onClick={createBooking}>+ new</button>
       <ul>
-        {todos.map((todo) => (
+        {bookings.map((booking) => (
           <li 
-          onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>{todo.content}
+          onClick={() => deleteBooking(booking.id)}
+          key={booking.id}>{booking.content}
           </li>
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 App successfully hosted. Try creating a new booking.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
       </div>
       <button onClick={signOut}>Sign out</button>
     </main>
